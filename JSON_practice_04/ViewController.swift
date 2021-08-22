769502e335e97eb38998dea6bcb3a952f9198b76
R-Cole <randy@goodglue.com>
2021-08-22 15:06:22 -0700

    image switching

--- JSON_practice_04/ViewController.swift
+++ JSON_practice_04/ViewController.swift
@@ -9,37 +9,42 @@ import UIKit
 import Foundation
  
 class ViewController: UIViewController {
+    
+    
+    let frisbeeImage = "frisbee-1.png"
+    let officeImage = "office-1.png"
+    let floridaImage = "florida-1.png"
  
     @IBOutlet weak var userName: UILabel!
 
     @IBOutlet weak var ageGuessed: UILabel!
 
-    @IBOutlet weak var userInout: UITextField!
+    @IBOutlet weak var userInput: UITextField!
     
-    
-    @IBAction func goGet(_ sender: Any) {
-        
-        guard let theInout = self.userInout.text else{ return }
-        
-        goGetStuff(theName: self.userInout.text!)
+    @IBAction func goGet(_ sender: UIButton) {
+         
+        guard let theInput = self.userInput.text else { return }
+         
+        goGetStuff(theName: theInput)
          
     }
+    
+    @IBOutlet weak var AgeImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //...
-         
-        
+          
     }
     
     
     func goGetStuff(theName: String) {
-        
+         
         var urlString = "https://api.agify.io/?name="
           
         guard let url = URL(string: urlString + theName) else { return }
-        
+         
         let task = URLSession.shared.dataTask(with: url, completionHandler: {
             
             data, response, error in
@@ -51,22 +56,40 @@ class ViewController: UIViewController {
             do {
                 
                 result = try JSONDecoder().decode(theResponse.self, from: theData)
-                
-                
+               
             } catch {
                 
                 print("failed to decode \(error)")
                 
             }
             
-            DispatchQueue.main.async {
-                
+            DispatchQueue.main.async { [self] in
+                 
                 guard let theResult = result else { return }
                 
                 self.userName.text = theResult.name
                 self.ageGuessed.text = String(theResult.age) + " years old"
                 
-                self.userInout.text = ""
+                self.userInput.text = ""
+                
+                
+                
+                if theResult.age < 30 {
+                     
+                    self.AgeImage.image = UIImage(named: frisbeeImage)
+                    
+                }
+                
+                if theResult.age >= 30 && theResult.age < 61 {
+                    
+                    self.AgeImage.image = UIImage(named: officeImage)
+                    
+                }
+                
+                if theResult.age >= 60 {
+                    
+                    self.AgeImage.image = UIImage(named: floridaImage)
+                }
             }
              
             
@@ -75,9 +98,7 @@ class ViewController: UIViewController {
         task.resume()
         
     }
-
-
-
+ 
 }
 
 
