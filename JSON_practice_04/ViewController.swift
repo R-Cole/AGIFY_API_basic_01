45e38b5cf757e2919438d6d42aeecc1742744d24
R-Cole <randy@goodglue.com>
2021-08-22 13:55:45 -0700

    first commit

--- JSON_practice_04/ViewController.swift
+++ JSON_practice_04/ViewController.swift
@@ -6,14 +6,85 @@
 //
 
 import UIKit
-
+import Foundation
+ 
 class ViewController: UIViewController {
+ 
+    @IBOutlet weak var userName: UILabel!
 
+    @IBOutlet weak var ageGuessed: UILabel!
+
+    @IBOutlet weak var userInout: UITextField!
+    
+    
+    @IBAction func goGet(_ sender: Any) {
+        
+        guard let theInout = self.userInout.text else{ return }
+        
+        goGetStuff(theName: self.userInout.text!)
+         
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //...
+         
+        
+    }
+    
+    
+    func goGetStuff(theName: String) {
+        
+        var urlString = "https://api.agify.io/?name="
+          
+        guard let url = URL(string: urlString + theName) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url, completionHandler: {
+            
+            data, response, error in
+            
+            guard let theData = data, error == nil else { print ("error \(error)"); return }
+            
+            var result: theResponse?
+            
+            do {
+                
+                result = try JSONDecoder().decode(theResponse.self, from: theData)
+                
+                
+            } catch {
+                
+                print("failed to decode \(error)")
+                
+            }
+            
+            DispatchQueue.main.async {
+                
+                guard let theResult = result else { return }
+                
+                self.userName.text = theResult.name
+                self.ageGuessed.text = String(theResult.age) + " years old"
+                
+                self.userInout.text = ""
+            }
+             
+            
+        })
+        
+        task.resume()
+        
     }
 
 
+
+}
+
+
+struct theResponse: Codable {
+    
+    var name: String
+    var age: Int
+    
 }
 
